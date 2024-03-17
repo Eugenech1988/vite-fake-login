4e2ee186683427543973a8c3fa774da898a0a71a
Yevhenii Chunachenko <yevhenii.chumachenko@norse.digital>
2024-03-17 13:19:45 +0200

    logo box styles fix

--- src/components/LogoBox/index.tsx
+++ src/components/LogoBox/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import logo from '../../assets/logo.svg';
 
-const LogoBox:React.FC = () => {
+const LogoBox: React.FC = () => {
+  const mediaMatch = window.matchMedia('(max-width: 768px)');
+  const [matches, setMatches] = useState(mediaMatch.matches);
+
+  useEffect(() => {
+    const handler = e => setMatches(e.matches);
+    mediaMatch.addListener(handler);
+    return () => mediaMatch.removeListener(handler);
+  });
+
+  const styles = {
+    box: (match) => {
+      if (match === false) {
+        return {
+          paddingTop: '180px',
+          marginBottom: '80px',
+          textAlign: 'center'
+        };
+      } else if (match === true) {
+        return {
+          paddingTop: '60px',
+          marginBottom: '40px',
+          textAlign: 'center'
+        };
+      }
+
+    }
+  };
+
   return (
-    <Box textAlign="center" paddingTop="180px" marginBottom="80px">
-      {/*@ts-ignore*/}
-      <img align="center" src={logo} alt="qencode"/>
-    </Box>
-  )
-}
+    <>
+      {/*ts-ignore*/}
+      <Box style={styles.box(matches)}>
+        <img src={logo} alt="qencode"/>
+      </Box>
+    </>
+  );
+};
 
 export default LogoBox;
